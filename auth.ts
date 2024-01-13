ff8964d50b3572b6db4857f6d592e4ff8c318027
Aman0430 <112618669+Aman0430@users.noreply.github.com>
2024-01-13 11:32:21 +0530

    feat: login and signup

--- auth.ts
+++ auth.ts
@@ -1,12 +1,14 @@
 import NextAuth from "next-auth"
 
 import { db } from "@/lib/db"
-import authConfig from "./auth.config"
+import authConfig from "@/auth.config"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 
 export const {
     handlers: { GET, POST },
     auth,
+    signIn,
+    signOut
 } = NextAuth({
     adapter: PrismaAdapter(db),
     session: { strategy: "jwt" },
